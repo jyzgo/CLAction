b0f1b63d7fe85b527025560af45833b34427652a
jyzgo <jiangyizheng@magictavern.com>
2015-08-28 16:49:37 +0800

    init

---
+++ MoveBy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBy : ActionInterval {
+
+	Vector3 _speed;
+	Vector3 _deltaPos;
+	public MoveBy(float duration,Vector3 deltaPos)
+	{
+		_deltaPos = deltaPos;
+
+		Debug.Assert(duration != 0f);
+		_speed = deltaPos/duration;
+
+	}
+
+	public override void excute()
+	{
+
+		updatePos();
+
+	}
+
+
+	void updatePos()
+	{
+		target.transform.Translate(_speed * Time.deltaTime);
+	}
+}
